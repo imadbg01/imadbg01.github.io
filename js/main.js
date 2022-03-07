10f075fcaeabef4f7a33610c0f3008c39611521c
imadbg01 <imadbg01@users.noreply.github.com>
2022-03-08 00:15:23 +0100

    play around  :heavy_check_mark:

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const firstFunction = () => {
+  // do something
+};
+
+const secondFunction = (a, b) => {
+  return a - b;
+};
+
+const authorFunction = () => {
+  return {
+    // return some object
+  };
+};
