f036e862d4ef9511f9c0e1c1e5504e6b942906bd
imad bg <imadbg01@users.noreply.github.com>
2022-08-02 01:46:33 +0100

    frish start again

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+const htmlTheme = document.querySelector('html')
+const themeToggle = document.querySelector("#theme-toggle")
+
+themeToggle.addEventListener("change", switchTheme)
+
+if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  htmlTheme.setAttribute("data-theme", "dark")
+  themeToggle.checked = true
+} else {
+  htmlTheme.setAttribute("data-theme", "light")
+  themeToggle.checked = false
+}
+
+function switchTheme() {
+  const theme = htmlTheme.getAttribute("data-theme")
+  if (theme === "light") {
+    htmlTheme.setAttribute("data-theme", "dark")
+    htmlTheme.classList.toggle("fadeIn")
+  } else {
+    htmlTheme.setAttribute("data-theme", "light")
+    htmlTheme.classList.toggle("fadeIn")
+
+  }
+}
+
+
 const firstFunction = () => {
   // do something
 };
